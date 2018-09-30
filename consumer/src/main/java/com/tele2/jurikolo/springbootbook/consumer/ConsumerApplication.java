c2e93d00e1120ecd7b5289133ee2b32278bde3e8
Jurijs Kolomijecs <jurijs.kolomijecs@tele2.com>
2018-09-30 19:42:29 +0300

    Added retry functionality

--- consumer/src/main/java/com/tele2/jurikolo/springbootbook/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/tele2/jurikolo/springbootbook/consumer/ConsumerApplication.java
@@ -9,9 +9,11 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.http.client.support.BasicAuthorizationInterceptor;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableRetry
 public class ConsumerApplication {
 
     @Bean
