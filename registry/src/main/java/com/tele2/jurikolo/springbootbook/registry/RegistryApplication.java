738731b20b4fb64cecdd7b34f02b21c01cbb55ed
Jurijs Kolomijecs <jurijs.kolomijecs@tele2.com>
2018-10-01 19:05:53 +0300

    Initial eureka service discovery

---
+++ registry/src/main/java/com/tele2/jurikolo/springbootbook/registry/RegistryApplication.java
@@ -0,0 +1,14 @@
+package com.tele2.jurikolo.springbootbook.registry;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class RegistryApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(RegistryApplication.class).web(WebApplicationType.SERVLET).run(args);
+    }
+}
